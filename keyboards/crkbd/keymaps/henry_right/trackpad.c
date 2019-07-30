d6f6f1631eb136d773074399cdc602d2e28ece35
jian <jian@socar.kr>
2019-07-30 17:26:55 +0900

    뭔가 relative한 값을 받아오는 것 같은데 dr처리를 해야 함.

--- keyboards/crkbd/keymaps/henry_right/trackpad.c
+++ keyboards/crkbd/keymaps/henry_right/trackpad.c
@@ -13,7 +13,7 @@ extern report_mouse_t mouse_report;
 
 // Register config values for this demo
 #define SYSCONFIG_1   0x00
-#define FEEDCONFIG_1  0x03
+#define FEEDCONFIG_1  0x01
 #define FEEDCONFIG_2  0x1F
 #define Z_IDLE_COUNT  0x05
 
@@ -31,42 +31,21 @@ void move_pointer(int x){
     pointing_device_send();
 }
 
-uint8_t last_result = 13;
-
 // Writes single-byte <data> to <address>
 void RAP_Write(uint8_t address, uint8_t data)
 {
   uint8_t cmdByte = WRITE_MASK | address;  // Form the WRITE command byte
 
-  SPI_Init(SPI_SPEED_FCPU_DIV_16 | SPI_MODE_MASTER | SPI_SCK_LEAD_RISING | SPI_SAMPLE_TRAILING);
   Assert_CS();
-  wait_ms(1);
 
   SPI_TransferByte(cmdByte);  // Signal a write to register at <address>
-  last_result = SPI_TransferByte(data);    // Send <value> to be written to register
+  SPI_TransferByte(data);     // Send <value> to be written to register
 
-  wait_ms(1);
   DeAssert_CS();
 
 }
 
 
-// Reads single-byte <data> from <address>
-uint8_t RAP_Read(uint8_t address)
-{
-  uint8_t cmdByte = READ_MASK | address;  // Form the WRITE command byte
-
-  Assert_CS();
-  wait_ms(1);
-
-  SPI_TransferByte(cmdByte);  // Signal a write to register at <address>
-  uint8_t data = SPI_TransferByte(0xfb);    // Send <value> to be written to register
-
-  wait_ms(1);
-  DeAssert_CS();
-
-  return data;
-}
 
 
 // Clears Status1 register flags (SW_CC and SW_DR)
@@ -75,22 +54,7 @@ void Pinnacle_ClearFlags(void){
   wait_ms(50);
 }
 
-/*  Pinnacle-based TM040040 Functions  */
-void Pinnacle_Init(void)
-{
-  DeAssert_CS();
-  wait_ms(10);
-
-  wait_ms(10);
-  SPI_Init(SPI_SPEED_FCPU_DIV_16 | SPI_MODE_MASTER | SPI_SCK_LEAD_RISING | SPI_SAMPLE_TRAILING);
-
-  wait_ms(10);
-  TP_CS_INIT;
-  DDRD &= ~(1 << 3);
-  //pinMode(DR_PIN, INPUT);
-
-
-
+void level_shift_init(void){
   LVL_SHIFT_EN_INIT;
   wait_ms(10);
 
@@ -99,8 +63,18 @@ void Pinnacle_Init(void)
   wait_ms(10);
   LVL_SHIFT_EN_HI;
   wait_ms(10);
+}
 
+void spi_init(void){
+  TP_CS_INIT;
+  DeAssert_CS();
+  SPI_Init(SPI_SPEED_FCPU_DIV_4 | SPI_MODE_MASTER | SPI_SCK_LEAD_RISING | SPI_SAMPLE_TRAILING);
+}
 
+void glidepoint_circle_init(void){
+  TP_CS_INIT;
+  // data ready pin init
+  DDRD &= ~(1 << 3);
 
   // Host clears SW_CC flag
   Pinnacle_ClearFlags();
@@ -118,27 +92,98 @@ void Pinnacle_Init(void)
 }
 
 
-
-
-
-
 void pointing_device_init(void){
-  Pinnacle_Init();
+  level_shift_init();
+  spi_init();
+  glidepoint_circle_init();
 }
 
-void pointing_device_task(void){
-  report_mouse_t currentReport = pointing_device_get_report();
-  if(last_result == 0xff) move_pointer(1);
-  if (isScrollMode){}
-  if(DR_Asserted())
+
+// Convenient way to store and access measurements
+typedef struct _absData
+{
+  uint16_t xValue;
+  uint16_t yValue;
+  uint16_t zValue;
+  uint8_t buttonFlags;
+  bool touchDown;
+} absData_t;
+
+typedef struct _relData
+{
+  int8_t xValue;
+  int8_t yValue;
+  uint8_t buttonFlags;
+  bool touchDown;
+} relData_t;
+
+relData_t touchData;
+
+/*  RAP Functions */
+// Reads <count> Pinnacle registers starting at <address>
+void RAP_ReadBytes(uint8_t address, uint8_t * data, uint8_t count)
+{
+  uint8_t cmdByte = READ_MASK | address;   // Form the READ command byte
+
+  Assert_CS();
+
+  SPI_TransferByte(cmdByte);  // Signal a RAP-read operation starting at <address>
+  SPI_TransferByte(0xFC);     // Filler byte
+  SPI_TransferByte(0xFC);     // Filler byte
+  for(uint8_t i = 0; i < count; i++)
   {
-    currentReport.x = 0;
-    currentReport.y = 0;
+    data[i] = SPI_TransferByte(0xFC);  // Each subsequent SPI transfer gets another register's contents
+  }
+
+  DeAssert_CS();
+}
+
+
+// Reads XYZ data from Pinnacle registers 0x14 through 0x17
+// Stores result in absData_t struct with xValue, yValue, and zValue members
+void Pinnacle_GetAbsolute(absData_t * result)
+{
+  uint8_t data[6] = { 0,0,0,0,0,0 };
+  RAP_ReadBytes(0x12, data, 6);
+
+  Pinnacle_ClearFlags();
+
+  result->buttonFlags = data[0] & 0x3F;
+  result->xValue = data[2] | ((data[4] & 0x0F) << 8);
+  result->yValue = data[3] | ((data[4] & 0xF0) << 4);
+  result->zValue = data[5] & 0x3F;
+
+  result->touchDown = result->xValue != 0;
+}
+
+void Pinnacle_GetRelative(relData_t * result)
+{
+  uint8_t data[4] = { 0,0,0,0 };
+  RAP_ReadBytes(0x12, data, 4);
+
+  Pinnacle_ClearFlags();
+
+  if((data[0] & 0xc8) != 0x08 ) move_pointer(10);
+
+  result->xValue = (int8_t)data[1];
+  result->yValue = (int8_t)data[2];
+}
+
+
+void pointing_device_task(void){
+  touchData.xValue = touchData.yValue = 0;
+
+  Pinnacle_GetRelative(&touchData);
+
+  report_mouse_t currentReport = pointing_device_get_report();
+  if (isScrollMode){}
+  //if(DR_Asserted())
+    currentReport.x = touchData.xValue;
+    currentReport.y = touchData.yValue;
 
     currentReport.buttons = mouse_report.buttons;
     pointing_device_set_report(currentReport);
     pointing_device_send();
-  }
 }
 
 
