ff7da5b90e6f74746974509852a32c62d9efc710
jian <jian@socar.kr>
2019-08-06 13:49:16 +0900

    안쓰게 된거 지우는 중

--- keyboards/crkbd/keymaps/henry_right/trackpad.c
+++ keyboards/crkbd/keymaps/henry_right/trackpad.c
@@ -5,25 +5,10 @@
 
 
 bool isScrollMode = false;
+touchData_t touchData;
 extern report_mouse_t mouse_report;
 
-#define Assert_CS() TP_CS_LO
-#define DeAssert_CS() TP_CS_HI
-
-// Masks for Cirque Register Access Protocol (RAP)
-#define WRITE_MASK  0x80
-#define READ_MASK   0xA0
-
-// Register config values for this demo
-#define SYSCONFIG_1   0x00
-#define FEEDCONFIG_1  0x01
-#define FEEDCONFIG_2  0x1F
-#define Z_IDLE_COUNT  0x05
-
-bool DR_Asserted(void){
-  return (PIND & (1 << 3));
-}
-
+///// TMP FIELD ////
 void move_pointer(int x){
   report_mouse_t currentReport = pointing_device_get_report();
     currentReport.x = x;
@@ -33,9 +18,6 @@ void move_pointer(int x){
     pointing_device_set_report(currentReport);
     pointing_device_send();
 }
-
-
-
 
 void level_shift_init(void){
   LVL_SHIFT_EN_INIT;
@@ -47,9 +29,7 @@ void level_shift_init(void){
   LVL_SHIFT_EN_HI;
   wait_ms(10);
 }
-
-
-touchData_t touchData;
+//// END TMP FIELD ////
 
 void pointing_device_init(void){
   level_shift_init();
@@ -60,16 +40,13 @@ void pointing_device_init(void){
   Pinnacle_init(&touchData, 0);
 }
 
-
 void pointing_device_task(void){
-  if (isScrollMode){
-
-  }
+  if (isScrollMode){}
 
   static int cnt = 0;
   ++cnt;
 
-  if((cnt % 8 == 0) || DR_Asserted())
+  if((cnt % 8 == 0) || HW_drAsserted(0))
   {
     if(touchData.mode == RELATIVE){
       touchData.relative.xDelta = touchData.relative.yDelta = 0;
